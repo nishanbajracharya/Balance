a2de049ced240c75b73a685a0a3812eb893b1dc2
Nishan Bajracharya <nisbaj11@gmail.com>
2018-06-10 21:28:44 +0545

    Add Text input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,57 @@
 import React from 'react';
 import { Input as RNInput } from 'react-native-elements';
 
-const Input = ({ ...props }) => <RNInput {...props} />;
+import fonts from '../constants/fonts';
+import colors from '../constants/colors';
+
+const styles = {
+  container: {
+    margin: 20
+  },
+  inputContainer: {
+    borderColor: colors.text.placeholder
+  },
+  inputContainerActive: {
+    borderBottomWidth: 2,
+    borderColor: colors.palette.primary
+  },
+  input: {
+    marginLeft: 0,
+    fontFamily: fonts.regular,
+    color: colors.text.paragraph,
+  },
+  error: {
+    marginLeft: 0,
+    fontFamily: fonts.regular,
+    color: colors.messages.error
+  }
+}
+
+class Input extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isActive: false
+    }
+  }
+
+  render() {
+    return <RNInput
+      {...this.props}
+      errorStyle={styles.error}
+      inputStyle={styles.input}
+      containerStyle={styles.container}
+      placeholderTextColor={colors.text.placeholder}
+      onFocus={() => this.setState({ isActive: true })}
+      onBlur={() => this.setState({ isActive: false })}
+      inputContainerStyle={this.state.isActive ? styles.inputContainerActive : styles.inputContainer}
+    />;
+  }
+}
+
+Input.defaultProps = {
+  placeholder: 'Input'
+}
 
 export default Input;
--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ export default class App extends Component {
           <Button flat />
           <Button disabled={true} />
           <Button flat disabled={true} />
-          <Input placeholder='BASIC INPUT'/>
+          <Input />
         </ScrollView>
       </View>
     );
